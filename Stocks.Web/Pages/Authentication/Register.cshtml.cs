45fb4ae115f7b3597db0a9a0948e003e2d016509
nightblade9 <nightbladecodes@gmail.com>
2023-12-10 23:10:44 -0500

    Registration form + validation + errors appear on POST

--- Stocks.Web/Pages/Authentication/Register.cshtml.cs
+++ Stocks.Web/Pages/Authentication/Register.cshtml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace Stocks.Web.Pages.Authentication;
 
 public class RegisterModel : PageModel
 {
+    private const int MinimumPasswordLength = 12;
+
+    [BindProperty]
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email Address")]
     public string EmailAddress { get; set; }
+    
+    [BindProperty]
+    [Required]
+    [MinLength(12)]
     public string Password { get; set; }
+
+    [BindProperty]
+    [Display(Name = "Retype Password")]
+    [Required]
+    [MinLength(12)]
+    [Compare(nameof(Password), ErrorMessage = "Passwords don't match.")] // doesn't work
     public string PasswordAgain { get; set; }
     
     private readonly ILogger<IndexModel> _logger;
@@ -27,8 +45,22 @@ public class RegisterModel : PageModel
 
     }
 
-    public void OnPost(string emailAddress, string password, string passwordAgain)
+    public async Task<IActionResult> OnPostAsync()
     {
-        ;
+        if (Password != PasswordAgain)
+        {
+            ModelState.AddModelError(nameof(Password), "Passwords don't match.");
+        }
+
+        await Task.CompletedTask;
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        // Save to DB here
+        // return RedirectToPage(...)
+        return Page();
     }
 }
