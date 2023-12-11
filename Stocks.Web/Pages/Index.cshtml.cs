ab2b8e197da2451b534fc48894137ae3cf8aa0f4
nightblade9 <nightbladecodes@gmail.com>
2023-12-10 20:14:49 -0500

    Move index code into code-behind

--- Stocks.Web/Pages/Index.cshtml.cs
+++ Stocks.Web/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Stocks.Web.Pages;
@@ -9,6 +10,13 @@ public class IndexModel : PageModel
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+    }
+
+    public override void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+    {
+        base.OnPageHandlerExecuted(context);
+
+        ViewData["Title"] = "Home | Stocks Spectator";
     }
 
     public void OnGet()
