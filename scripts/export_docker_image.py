bcc41164d1b26d28bbe85ac44dcca933eeec0114
nightblade9 <nightbladecodes@gmail.com>
2023-12-14 00:09:53 -0500

    Fix: export script

--- scripts/export_docker_image.py
+++ scripts/export_docker_image.py
@@ -4,4 +4,4 @@ import subprocess
 
 DOCKER_IMAGE_NAME = "stocks-web-image"
 date = datetime.datetime.now().strftime('%Y%m%d-%H%M') 
-subprocess.call(f"docker save -o latest_image.tar ../{DOCKER_IMAGE_NAME}-{date}")
+subprocess.call(f"docker save -o ../latest_image-{date}.tar {DOCKER_IMAGE_NAME}")
