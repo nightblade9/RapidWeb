e5bbf04c947318d585947c8367955500106955c6
nightblade9 <nightbladecodes@gmail.com>
2024-08-13 17:17:10 -0400

    Unit-test infrastructure + dummy test.

---
+++ WebApp.Web.UnitTests/Pages/ErrorTests.cs
@@ -0,0 +1,13 @@
+namespace WebApp.Web.UnitTests;
+
+using NUnit.Framework;
+
+[TestFixture]
+public class ErrorTests
+{
+    [Test]
+    public void Lol_Wut()
+    {
+        Assert.That(1 + 2, Is.EqualTo(3));
+    }
+}
