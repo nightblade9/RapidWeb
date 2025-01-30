ea8975b21b58b9e2e2fcde0567d3f070920b4622
nightblade9 <nightbladecodes@gmail.com>
2025-01-30 14:15:46 -0500

    Code coverage: ignore Program.cs

--- WebApp.Web/Program.cs
+++ WebApp.Web/Program.cs
@@ -1,86 +1,96 @@
+using System.Diagnostics.CodeAnalysis;
 using FluentMigrator.Runner;
 using WebApp.DataAccess;
 using WebApp.DataAccess.Authentication;
 using WebApp.DataAccess.HealthCheck;
 using WebApp.DataAccess.Migration.Migrations;
 
-/// Uses an implicit partial class. Manually exclude from code coverage on the tool side.
-/// In SonarQube, add it to general file exclusions (**/Program.cs)
+namespace WebApp.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// Routing
+[ExcludeFromCodeCoverage]
+public class Program
 {
-    builder.Services.AddRazorPages(options => {
-        options.Conventions.AddPageRoute("/Authentication/Login", "login");
-        options.Conventions.AddPageRoute("/Authentication/Register", "register");
-    });
-}
-
-// add services to DI container
-{
-    var services = builder.Services;
-    services.AddCors();
-}
-
-// DB config
-var connectionString = builder.Configuration.GetConnectionString("Default");
-{
-    if (string.IsNullOrWhiteSpace(connectionString))
+    public async static Task Main(string[] args)
     {
-        throw new InvalidOperationException("Please define a connection string in your config.");
+        /// Uses an implicit partial class. Manually exclude from code coverage on the tool side.
+        /// In SonarQube, add it to general file exclusions (**/Program.cs)
+
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        // Routing
+        {
+            builder.Services.AddRazorPages(options => {
+                options.Conventions.AddPageRoute("/Authentication/Login", "login");
+                options.Conventions.AddPageRoute("/Authentication/Register", "register");
+            });
+        }
+
+        // add services to DI container
+        {
+            var services = builder.Services;
+            services.AddCors();
+        }
+
+        // DB config
+        var connectionString = builder.Configuration.GetConnectionString("Default");
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Please define a connection string in your config.");
+            }
+
+            // Dependency injection for our app
+            builder.Services.AddTransient(x => new DatabaseConnection(connectionString));
+            builder.Services.AddTransient<IConnectionChecker, ConnectionChecker>();
+            builder.Services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
+
+            // DB Migrations runner config
+            builder.Services.AddFluentMigratorCore().ConfigureRunner(r => r
+                // .AddMySql5()
+                .AddSQLite()
+                // Migrations connection string
+                .WithGlobalConnectionString(connectionString)
+                // Assembly we check for migrations 
+                .ScanIn(typeof(AddUsersTable_001).Assembly).For.Migrations())
+                .AddLogging(l => l.AddFluentMigratorConsole());
+        };
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        // Run migrations
+        using (var migratorScope = app.Services.CreateScope())
+        {
+            var services = migratorScope.ServiceProvider;
+            var migrationRunner = services.GetService<IMigrationRunner>();
+
+            if (migrationRunner == null)
+            {
+                return;
+            }
+
+            migrationRunner.ValidateVersionOrder();
+            migrationRunner.MigrateUp();
+        }
+
+        await app.RunAsync();
     }
-
-    // Dependency injection for our app
-    builder.Services.AddTransient(x => new DatabaseConnection(connectionString));
-    builder.Services.AddTransient<IConnectionChecker, ConnectionChecker>();
-    builder.Services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
-
-    // DB Migrations runner config
-    builder.Services.AddFluentMigratorCore().ConfigureRunner(r => r
-        // .AddMySql5()
-        .AddSQLite()
-        // Migrations connection string
-        .WithGlobalConnectionString(connectionString)
-        // Assembly we check for migrations 
-        .ScanIn(typeof(AddUsersTable_001).Assembly).For.Migrations())
-        .AddLogging(l => l.AddFluentMigratorConsole());
-};
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-// Run migrations
-using (var migratorScope = app.Services.CreateScope())
-{
-    var services = migratorScope.ServiceProvider;
-    var migrationRunner = services.GetService<IMigrationRunner>();
-
-    if (migrationRunner == null)
-    {
-        return;
-    }
-
-    migrationRunner.ValidateVersionOrder();
-    migrationRunner.MigrateUp();
-}
-
-await app.RunAsync();
