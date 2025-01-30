5787c75cd5ed235753e3a569c649b0413b08d31a
nightblade9 <nightbladecodes@gmail.com>
2025-01-30 17:00:16 -0500

    Simplify privacy page in-page logic

--- WebApp.Web/Pages/Privacy.cshtml.cs
+++ WebApp.Web/Pages/Privacy.cshtml.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Web.Pages;
 
 public class PrivacyModel : PageModel
 {
+    public override void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+    {
+        base.OnPageHandlerExecuted(context);
+        ViewData["Title"] = "Privacy Policy";
+    }
 }
 
